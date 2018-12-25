6b901706715fac8de3df2c7eab660de8712bf959
Aceskua <742965106@qq.com>
2018-12-25 20:29:38 +0800

    7-15 over

--- ReaderMovie/pages/movies/movies.js
+++ ReaderMovie/pages/movies/movies.js
@@ -1,65 +1,115 @@
+var util = require('../../utils/util.js');
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    inTheaters: {},
+    comingSoon: {},
+    top250: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+    var inTheatersUrl = app.globalData.g_doubanBase + '/v2/movie/in_theaters' + '?start=0&count=3';
+    var comingSoonUrl = app.globalData.g_doubanBase + '/v2/movie/coming_soon' + '?start=0&count=3';
+    var top250Url = app.globalData.g_doubanBase + '/v2/movie/top250' + '?start=0&count=3';
+    this.getMovieListData(inTheatersUrl, 'inTheaters');
+    this.getMovieListData(comingSoonUrl, 'comingSoon');
+    this.getMovieListData(top250Url, 'top250');
+  },
+
+  getMovieListData: function(url, settedKey) {
+    var that = this;
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        "Content-Type": "json"
+      },
+      success: function(res) {
+        console.log(res);
+        that.processDoubanData(res.data, settedKey);
+      }
+    })
+  },
+
+  processDoubanData: function(moviesDouban, settedKey) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + '...';
+      }
+      var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp);
+    }
+
+    var readyData = {};
+    readyData[settedKey] = {
+      movies: movies
+    };
+    this.setData(readyData);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
